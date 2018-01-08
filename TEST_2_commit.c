5c3f49da02804456f209d11ec77321dd270d4c5d
Daniel Mohazab <34020691+dmohazab@users.noreply.github.com>
2018-01-08 13:10:24 -0500

    Update TEST_2_commit.c
    styling

--- TEST_2_commit.c
+++ TEST_2_commit.c
@@ -1,26 +1,26 @@
-/* This test code allowed us to find the appropriate encoder
+/* This driver allowed us to find the appropriate encoder
  limit for the chord commission system.*/
 
 task main()
 {
 	while(!getButtonPress(buttonAny))
-    {}
+    	{}
     
-    nMotorEncoder[motorA] = 0;
+	nMotorEncoder[motorA] = 0;
 	motor[motorA] = -30;
 
 	while (getButtonPress(buttonAny))
 	{}
     
-    motor[motorA] = 0;
-    displayString(0, "Encoder Value: %i", nMotorEncoder[motorA]);
+	motor[motorA] = 0;
+    	displayString(0, "Encoder Value: %i", nMotorEncoder[motorA]);
 	
-    motor[motorA] = 30;
+    	motor[motorA] = 30;
     
-    while(nMotorEncoder[motorA] < 0)
-    {}
+    	while(nMotorEncoder[motorA] < 0)
+    	{}
 
 	motor[motorA] = 0;
     
-    wait1Msec(2000);
+   	wait1Msec(2000);
 }
